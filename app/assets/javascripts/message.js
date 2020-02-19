8b5ec2999ebad16e1084ac339bd4fa7deefbd1b3
takehiro37 <takehiro37@gmail.com>
2020-02-19 10:05:54 +0900

    Fixed

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,16 +3,16 @@ $(function(){
     if ( message.image ) {
       var html =
         `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
+          <div class="message__upper-info">
+            <div class="message__upper-info__talker">
               ${message.user_name}
             </div>
-            <div class="upper-message__date">
+            <div class="message__upper-info__date">
               ${message.date}
             </div>
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
+          <div class="message__text">
+            <p class="message__text__image">
               ${message.content}
             </p>
           </div>
@@ -23,16 +23,16 @@ $(function(){
     else {
       var html =
         `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
+          <div class="message__upper-info">
+            <div class="message__upper-info__talker">
               ${message.user_name}
             </div>
-            <div class="upper-message__date">
+            <div class="message__upper-info__date">
               ${message.date}
             </div>
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
+          <div class="message__text">
+            <p class="message__text__image">
               ${message.content}
             </p>
           </div>
@@ -40,27 +40,30 @@ $(function(){
       return html;
     };
   }
-$('.js-form').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data);
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(function() {
+      $('.form__submit-btn').prop('disabled', false);
+    });
   })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-    $('form')[0].reset();
-  })
-  .fail(function(){
-     alert('error');
-  });
-   return false;
-});
 });
--- db/migrate/20200213033807_create_messages.rb
+++ db/migrate/20200213033807_create_messages.rb
@@ -5,6 +5,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
       t.string :image
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
+      t.timestamps
     end
   end
 end
